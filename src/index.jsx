8a2b280fc78adf1ec89a694ae0cd13c1c12cfe4f
Ramon Silva <ramonss.bque@gmail.com>
2023-12-12 18:12:03 -0300

    react-hooks first commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './templates/App';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Page404 } from './templates/Page404';
+import UseState from './components/useState/useState';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<UseState />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  </BrowserRouter>,
+);
